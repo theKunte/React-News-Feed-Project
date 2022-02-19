58182073ac1a6f61a9ef7a31b8072892a0e257b2
Jenny <jennykunte90@gmail.com>
2022-02-19 07:54:22 -0800

    set up newsfeed

---
+++ src/components/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import News from './News/News';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Jenny's News Feed</h1>
+        </header>
+       
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/News/NewSingle.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const NewSingle = ({item}) => (
+    <li>
+        <p>{item.title}</p>
+    </li>
+);
+export default NewSingle;
